cd12d68d30c907bde684c6639ce7d58b291e7ebe
Kubasov Alexei <kubasovalexei@yandex.ru>
2022-04-09 17:55:04 +0700

    Add first elements

--- src/main/java/ru/yandex/praktikum/MainPage.java
+++ src/main/java/ru/yandex/praktikum/MainPage.java
@@ -4,7 +4,15 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 
 public class MainPage {
-    // локатор поля ввода email
-    @FindBy(how = How.ID,using = "email")
-    private SelenideElement emailField;
+    // логотип Яндекса
+    @FindBy(how = How.CLASS_NAME,using = "Header_LogoYandex__3TSOI")
+    public SelenideElement yandexLogo;
+
+    // логотип Самоката
+    @FindBy(how = How.CLASS_NAME,using = "Header_LogoScooter__3lsAR")
+    public SelenideElement samokatLogo;
+
+    // нопка "Заказать" в шапке страницы
+    @FindBy(how = How.XPATH,using = "//div/div[1]/div[2]/button[1]")
+    public SelenideElement orderButtonHeader;
 }
--- src/test/java/ru/yandex/praktikum/MainTest.java
+++ src/test/java/ru/yandex/praktikum/MainTest.java
@@ -1,10 +1,15 @@
 package ru.yandex.praktikum;
 import org.junit.Test;
+import static com.codeborne.selenide.Selenide.open;
+
+import ru.yandex.praktikum.MainPage;
 
 public class MainTest {
 
     @Test
     public void lalala() {
-
+        MainPage mainPage = open("https://qa-scooter.praktikum-services.ru/", MainPage.class);
+        //mainPage.yandexLogo.click();
+        mainPage.orderButtonHeader.click();
     }
 }
