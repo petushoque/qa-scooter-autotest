c1db0c3385dc1cad4efbd5468c681fda9d84ee88
Kubasov Alexei <kubasovalexei@yandex.ru>
2022-04-10 10:13:42 +0700

    Update MainPage.java

--- src/main/java/ru/yandex/praktikum/MainPage.java
+++ src/main/java/ru/yandex/praktikum/MainPage.java
@@ -4,17 +4,28 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 
 public class MainPage {
-    // логотип Яндекса
+    // логотип Яндекса в шапке страницы
     @FindBy(how = How.CLASS_NAME,using = "Header_LogoYandex__3TSOI")
     public SelenideElement yandexLogo;
 
-    // логотип Самоката
+    // логотип Самоката в шапке страницы
     @FindBy(how = How.CLASS_NAME,using = "Header_LogoScooter__3lsAR")
-    public SelenideElement samokatLogo;
+    public SelenideElement scooterLogo;
 
-    // нопка "Заказать" в шапке страницы
-    @FindBy(how = How.XPATH,using = "//div/div[1]/div[2]/button[1]")
+    // дисклеймер "учебный тренажер" в шапке страницы
+    @FindBy(how = How.CLASS_NAME, using = "Header_Disclaimer__3VEni")
+    public SelenideElement headerDisclaimer;
+
+    // кнопка "Заказать" в шапке страницы
+    @FindBy(how = How.XPATH,using = "//div[@class='Header_Nav__AGCXC']/button[text()='Заказать']")
     public SelenideElement orderButtonHeader;
+
+    // кнопка "Статус заказа" в шапке страницы
+    @FindBy(how = How.XPATH, using = "//div[@class='Header_Nav__AGCXC']/button[text()='Статус заказа']")
+    public SelenideElement orderStatusButtonHeader;
+
+    // гланый title страницы
+    @FindBy(how = How)
 
     @FindBy(how = How.XPATH,using = "//div/div[2]/div[2]/div[1]/input")
     public SelenideElement nameFieldInOrderForm;
