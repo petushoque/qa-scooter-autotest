f097c50339f5a14c3d2bfda7d53a4add43c6d1e6
Kubasov Alexei <kubasovalexei@yandex.ru>
2022-04-10 09:52:03 +0700

    Update MainTest.java

--- src/test/java/ru/yandex/praktikum/MainTest.java
+++ src/test/java/ru/yandex/praktikum/MainTest.java
@@ -1,6 +1,7 @@
 package ru.yandex.praktikum;
 import org.junit.Test;
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.screenshot;
 
 import ru.yandex.praktikum.MainPage;
 
@@ -11,5 +12,7 @@ public class MainTest {
         MainPage mainPage = open("https://qa-scooter.praktikum-services.ru/", MainPage.class);
         //mainPage.yandexLogo.click();
         mainPage.orderButtonHeader.click();
+        mainPage.nameFieldInOrderForm.setValue("Test");
+        String pngFileName = screenshot("my_file_name");
     }
 }
