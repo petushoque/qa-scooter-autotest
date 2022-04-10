c521b440565267031080770e72998d584be056ba
Kubasov Alexei <kubasovalexei@yandex.ru>
2022-04-10 10:57:36 +0700

    Update MainPage.java

--- src/main/java/ru/yandex/praktikum/MainPage.java
+++ src/main/java/ru/yandex/praktikum/MainPage.java
@@ -24,8 +24,35 @@ public class MainPage {
     @FindBy(how = How.XPATH, using = "//div[@class='Header_Nav__AGCXC']/button[text()='Статус заказа']")
     public SelenideElement orderStatusButtonHeader;
 
-    // гланый title страницы
-    @FindBy(how = How)
+    // гланый title страницы, 1 строка
+    @FindBy(how = How.XPATH, using = "//div[@class='Home_Header__iJKdX']/text()[1]")
+    public SelenideElement titleFirstLine;
+
+    // главный title страницы, 2 строка
+    @FindBy(how = How.XPATH, using = "//div[@class='Home_Header__iJKdX']/text()[2]")
+    public SelenideElement titleSecondLine;
+
+    //НАЙТИ СУБХЭДЕРЫ
+    // первый subheader, 1 строка
+    //@FindBy(how = How.XPATH, using = "//div[@class='Home_SubHeader__zwi_E']/text()[1]")
+    //public SelenideElement titleSecondLine;
+
+    // картинка, схематическое изображение самоката
+    @FindBy(how = How.XPATH, using = "//div[@class='Home_BluePrint__TGX2n']/img[@alt='Scooter blueprint']")
+    public SelenideElement scooterBlueprintImage;
+
+    // картинка, фотография самоката
+    @FindBy(how = How.XPATH, using = "//div[@class='Home_Scooter__3YdJy']/img[@alt='Scooter blueprint']")
+    public SelenideElement scooterPhotoImage;
+
+    // subheader 3, "Как это работает"
+    @FindBy(how = How.XPATH, using = "//div[@class='Home_ThirdPart__LSTEE']/div[@class='Home_SubHeader__zwi_E']/text()")
+    public SelenideElement subheaderHowItWorks;
+
+    // "Как это работает", шаг 1, номер шага
+    @FindBy(how = How.XPATH, using = "//div[@class='Home_RoadMap__2tal_']/div[1]/div[@class='Home_StatusCircle__3_aTp']")
+    public SelenideElement numberOfFirstStep;
+
 
     @FindBy(how = How.XPATH,using = "//div/div[2]/div[2]/div[1]/input")
     public SelenideElement nameFieldInOrderForm;
