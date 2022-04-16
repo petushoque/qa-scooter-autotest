30822fbfbdd0d29e7d7f3114886e1efc60f040ed
Kubasov Alexei <kubasovalexei@yandex.ru>
2022-04-16 09:53:26 +0700

    Update TestQuestionsAccordion.java

--- src/test/java/ru/yandex/praktikum/TestQuestionsAccordion.java
+++ src/test/java/ru/yandex/praktikum/TestQuestionsAccordion.java
@@ -1,4 +1,13 @@
 package ru.yandex.praktikum;
+import org.junit.Test;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.screenshot;
 
 public class TestQuestionsAccordion {
+
+    @Test
+    public void shouldQuestionsCorrespondToAnswers() {
+        
+    }
 }
