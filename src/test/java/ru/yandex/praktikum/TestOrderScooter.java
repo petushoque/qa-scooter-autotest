53a5afd4422f24e0605fe0979a1651b3f26385ed
Kubasov Alexei <kubasovalexei@yandex.ru>
2022-04-16 09:53:24 +0700

    Update TestOrderScooter.java

--- src/test/java/ru/yandex/praktikum/TestOrderScooter.java
+++ src/test/java/ru/yandex/praktikum/TestOrderScooter.java
@@ -1,6 +1,5 @@
 package ru.yandex.praktikum;
 import org.junit.Test;
-
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.Selenide.screenshot;
