9e37fb8eba04abc7e04df84bccd40e87f6b11be4
Kubasov Alexei <kubasovalexei@yandex.ru>
2022-04-16 17:31:42 +0700

    Update Customer.java

--- src/main/java/ru/yandex/praktikum/model/Customer.java
+++ src/main/java/ru/yandex/praktikum/model/Customer.java
@@ -1,5 +1,17 @@
 package ru.yandex.praktikum.model;
 
 public class Customer {
-    
+    String firstName;
+    String lastName;
+    String orderAddress;
+    String metroStation;
+    String phoneNumber;
+
+    Customer(String firstName, String lastName, String orderAddress, String metroStation, String phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.orderAddress = orderAddress;
+        this.metroStation = metroStation;
+        this.phoneNumber = phoneNumber;
+    }
 }
