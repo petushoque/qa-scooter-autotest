9beb0d6d4a462ef6fdace0b33b458d3de8b2d845
Kubasov Alexei <kubasovalexei@yandex.ru>
2022-04-16 16:50:11 +0700

    Add a loop to check answers to questions

--- src/test/java/ru/yandex/praktikum/TestQuestionsAccordion.java
+++ src/test/java/ru/yandex/praktikum/TestQuestionsAccordion.java
@@ -1,10 +1,6 @@
 package ru.yandex.praktikum;
+import org.junit.Assert;
 import org.junit.Test;
-import org.openqa.selenium.By;
-
-import javax.swing.plaf.synth.SynthOptionPaneUI;
-
-import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.Selenide.screenshot;
 
@@ -13,10 +9,12 @@ public class TestQuestionsAccordion {
     @Test
     public void shouldQuestionsCorrespondToAnswers() {
         MainPage mainPage = open(MainPage.MAIN_PAGE_URL, MainPage.class);
-        mainPage.questionsAccordion.get(1).scrollIntoView(true).click();
-        String test = mainPage.getAnswerText(1);
-        System.out.println("Hello,");
-        System.out.println(test);
+
+        for(int i = 0; i < 8; i++){
+            mainPage.clickOnQuestion(i);
+            Assert.assertEquals(mainPage.getAnswerText(i), mainPage.ANSWERS[i]);
+        }
+
         String pngFileName = screenshot("my_file_name");
     }
 }
