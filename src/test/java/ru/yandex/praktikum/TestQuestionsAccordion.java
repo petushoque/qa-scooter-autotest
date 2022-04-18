3cf79791bb39e2ccc6921b7a48727c9b0c2280f6
Kubasov Alexei <kubasovalexei@yandex.ru>
2022-04-18 07:13:58 +0700

    Update TestQuestionsAccordion.java

--- src/test/java/ru/yandex/praktikum/TestQuestionsAccordion.java
+++ src/test/java/ru/yandex/praktikum/TestQuestionsAccordion.java
@@ -20,60 +20,52 @@ public class TestQuestionsAccordion {
 
     }
 
-
+    MainPage mainPage = open(MainPage.MAIN_PAGE_URL, MainPage.class);
 
     @Test
     public void shouldQuestionOneWillBeCorrect() {
-        MainPage mainPage = open(MainPage.MAIN_PAGE_URL, MainPage.class);
         mainPage.clickOnQuestion(0);
         Assert.assertEquals(mainPage.getAnswerText(0), mainPage.ANSWERS[0]);
     }
 
     @Test
     public void shouldQuestionTwoWillBeCorrect() {
-        MainPage mainPage = open(MainPage.MAIN_PAGE_URL, MainPage.class);
         mainPage.clickOnQuestion(1);
         Assert.assertEquals(mainPage.getAnswerText(1), mainPage.ANSWERS[1]);
     }
 
     @Test
     public void shouldQuestionThreeWillBeCorrect() {
-        MainPage mainPage = open(MainPage.MAIN_PAGE_URL, MainPage.class);
         mainPage.clickOnQuestion(2);
         Assert.assertEquals(mainPage.getAnswerText(2), mainPage.ANSWERS[2]);
     }
 
     @Test
     public void shouldQuestionFourWillBeCorrect() {
-        MainPage mainPage = open(MainPage.MAIN_PAGE_URL, MainPage.class);
         mainPage.clickOnQuestion(3);
         Assert.assertEquals(mainPage.getAnswerText(3), mainPage.ANSWERS[3]);
     }
 
     @Test
     public void shouldQuestionFiveWillBeCorrect() {
-        MainPage mainPage = open(MainPage.MAIN_PAGE_URL, MainPage.class);
         mainPage.clickOnQuestion(4);
         Assert.assertEquals(mainPage.getAnswerText(4), mainPage.ANSWERS[4]);
     }
 
     @Test
     public void shouldQuestionSixWillBeCorrect() {
-        MainPage mainPage = open(MainPage.MAIN_PAGE_URL, MainPage.class);
         mainPage.clickOnQuestion(5);
         Assert.assertEquals(mainPage.getAnswerText(5), mainPage.ANSWERS[5]);
     }
 
     @Test
     public void shouldQuestionSevenWillBeCorrect() {
-        MainPage mainPage = open(MainPage.MAIN_PAGE_URL, MainPage.class);
         mainPage.clickOnQuestion(6);
         Assert.assertEquals(mainPage.getAnswerText(6), mainPage.ANSWERS[6]);
     }
 
     @Test
     public void shouldQuestionEightWillBeCorrect() {
-        MainPage mainPage = open(MainPage.MAIN_PAGE_URL, MainPage.class);
         mainPage.clickOnQuestion(7);
         Assert.assertEquals(mainPage.getAnswerText(7), mainPage.ANSWERS[7]);
     }
